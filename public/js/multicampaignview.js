68f320d3bf6b3a8fa82826198848c8b13192e44f
MMMarz009 <mar0099887@gmail.com>
2020-08-02 17:39:25 -0500

    Updated Logic

--- public/js/multicampaignview.js
+++ public/js/multicampaignview.js
@@ -1,41 +1,29 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="Description" content="Enter your description here" />
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/lumen/bootstrap.min.css">
-    <link href="https://fonts.googleapis.com/css2?family=Almendra:wght@700&family=Metamorphous&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="./stylesheets/style.css">
-    <link href="./stylesheets/multiple.css" rel="stylesheet">
+$(document).ready(()=>{
+    $.get("/api/campaigns").then(dbCampaigns =>{
+        console.log(dbCampaigns);
 
-    <title>All Campaigns</title>
-</head>
 
-<body>
-    <nav class="navbar">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <a class="navbar-brand" href="members.html">
-                    Back to Menu
-                  </a>
-            </div>
-        </div>
-    </nav>
-    <div class="container">
-        <div class="tiles ">
-          <div class="col-md-6 col-md-offset-1">
-  
-            <div type="view-chara" class="tile multiple-desktop ">
-              <div class="multiple-desktop-text">
-                <a href="updatecampaign.html">
-                  Campaign 1
-                </a>
-              </div>
-            </div>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
-<script src="../node_modules/multiple.js/multiple.js"></script>
+        dbCampaigns.map(campaigns =>{
 
-</body>
-</html>
+        $("#campaigns-div").append(
+            $(`
+            <div class="tiles ">
+                <div type="view-chara" class="tile multiple-desktop ">
+                  <div class="multiple-desktop-text" id= ${campaigns.id}>
+                    <a href="updatecampaign.html">
+                      ${campaigns.name}
+                    </a>
+                  </div>
+                </div>
+            </div>`)
+        );
+    });
+
+
+       }).catch(err =>{
+         console.log("Why are you here, buddy?");
+          console.log(err);
+      });     
+              
+ });
+
